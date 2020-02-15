ac7525fbc363822cd102b5137693b30a3ab13cbb
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-02-15 12:37:20 +0900

    Express server
    ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("HI");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("you are my profile");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
