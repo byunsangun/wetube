213663b33856218bb978d9268c585c2b33652e52
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-02-16 20:20:01 +0900

    Router

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
 const app = express();
 
 const PORT = 4000;
@@ -24,4 +26,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
