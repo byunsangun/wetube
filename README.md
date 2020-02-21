74c1b70e7b655b86a4e51380adb4942642199d66
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-02-21 18:59:07 +0900

    Home Conroller
    mixin concept

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ cloning Youtube with Vanilla and Nodejs
 ## Pages:
 
 - [ ] Home
-- [ ] Join
-- [ ] Login
+- [x] Join
+- [x] Login
 - [x] Search
 - [ ] User Detail
 - [ ] Edit Profile
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 3232654,
+    title: "Video awesome",
+    description: "This is something i love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "sangun",
+      email: "xdcfvgg@naver.com"
+    }
+  },
+  {
+    id: 98458,
+    title: "Video super",
+    description: "This is something i love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "sangun",
+      email: "xdcfvgg@naver.com"
+    }
+  },
+  {
+    id: 751215,
+    title: "Video nice",
+    description: "This is something i love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "sangun",
+      email: "xdcfvgg@naver.com"
+    }
+  },
+  {
+    id: 55432,
+    title: "Video perfect",
+    description: "This is something i love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "sangun",
+      email: "xdcfvgg@naver.com"
+    }
+  }
+];
