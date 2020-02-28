129e8029b91d478c42e5e5cbba88b75754172393
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-02-28 18:20:19 +0900

    MongoStore

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -14,6 +16,8 @@ import { localsMiddleware } from "./middlewares";
 import "./passport";
 
 const app = express();
+
+const CokieStore = MongoStore(session);
 
 //middlewares
 app.use(helmet());
@@ -28,7 +32,8 @@ app.use(
   session({
     secret: process.env.COOKIE_SECRET,
     resave: true,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new CokieStore({ mongooseConnection: mongoose.connection })
   })
 );
 app.use(passport.initialize());
