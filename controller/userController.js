5bbf70638eb20484d7228d8b0a6395eda8d4d63e
byunsangun <48983864+byunsangun@users.noreply.github.com>
2020-02-28 20:21:56 +0900

    Github log-in

--- controller/userController.js
+++ controller/userController.js
@@ -39,8 +39,12 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
 export const logout = (req, res) => {
-  //To DO : Process Log Out
+  req.logout();
   res.redirect(routes.home);
 };
 
